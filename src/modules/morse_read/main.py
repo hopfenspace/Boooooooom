e033890aadceca5d93e8365a9c846e88ba8b3591
myOmikron <accounts@omikron.dev>
2021-10-22 19:37:23 +0200

    Implemented first (incomplete) version of morse_read

---
+++ src/modules/morse_read/main.py
@@ -0,0 +1,131 @@
+import time
+import random
+
+from machine import Pin
+
+
+class MorseReadGame:
+    def __init__(self, *, difficulty, wordlist_count):
+        self.difficulty = difficulty
+        self.wordlist_count = wordlist_count
+
+        self.international_morse_code = {
+            "a": ".-", "b": "-...", "c": "-.-.", "d": "-..", "e": ".", "f": "..-.", "g": "--.", "h": "....",
+            "i": "..", "j": ".---", "k": "-.-", "l": ".-..", "m": "--", "n": "-.", "o": "---", "p": ".--.",
+            "q": "--.-", "r": ".-.", "s": "...", "t": "-", "u": "..-", "v": "...-", "w": ".--", "x": "-..-",
+            "y": "-.--", "z": "--..", "1": ".----", "2": "..---", "3": "...--", "4": "....-", "5": ".....",
+            "6": "-....", "7": "--...", "8": "---..", "9": "----.", "0": "-----", " ": "/"
+        }
+
+        wordlist = self._get_wordlist()
+        frequency_list = set()
+        while len(frequency_list) != len(wordlist):
+            frequency_list.add(random.randint(1000, 2000))
+        self.frequency_list = list(frequency_list)
+        self.frequency_list.sort()
+        self.wordlist_lookup = {}
+        # Fill wordlist_lookup with kvp:    word: frequency
+        [self.wordlist_lookup.__setitem__(wordlist[x], self.frequency_list[x]) for x in range(len(wordlist))]
+        self.chosen_one = wordlist[random.randint(0, self.wordlist_count)]
+
+        self.led_morse = Pin(16, Pin.OUT)
+        self.led_strike = Pin(17, Pin.OUT)
+        self.led_solved = Pin(5, Pin.OUT)
+
+        self.button_left = Pin(4, Pin.IN)
+        self.button_left.irq(handler=self.handle_left, trigger=Pin.IRQ_RISING)
+
+        self.button_right = Pin(15, Pin.IN)
+        self.button_right.irq(handler=self.handle_right, trigger=Pin.IRQ_RISING)
+
+        self.button_submit = Pin(2, Pin.IN)
+        self.button_submit.irq(handler=self.handle_submit, trigger=Pin.IRQ_RISING)
+
+        self.current = self.wordlist_lookup[wordlist[random.randint(0, self.wordlist_count)]]
+        self.solved = False
+
+    def handle_left(self, *args, **kwargs):
+        if not self.solved:
+            current_index = self.frequency_list.index(self.current)
+            if current_index != 0:
+                self.current = self.wordlist_lookup[list(self.wordlist_lookup.keys())[current_index-1]]
+                # TODO Set frequency to self.current
+
+    def handle_right(self, *args, **kwargs):
+        if not self.solved:
+            current_index = self.frequency_list.index(self.current)
+            if current_index != len(self.frequency_list)-1:
+                self.current = self.wordlist_lookup[list(self.wordlist_lookup.keys())[current_index+1]]
+                # TODO Set frequency to self.current
+
+    def handle_submit(self, *args, **kwargs):
+        if not self.solved:
+            if self.wordlist_lookup[self.chosen_one] == self.current:
+                # TODO Send solved
+                self.led_solved.on()
+                self.solved = True
+            else:
+                # TODO Send strike
+                self.led_strike.on()
+                time.sleep_ms(200)
+                self.led_strike.off()
+
+    def _dot(self):
+        self.led_morse.on()
+        time.sleep_ms(self.difficulty)
+        self.led_morse.off()
+
+    def _dash(self):
+        self.led_morse.on()
+        time.sleep_ms(self.difficulty*3)
+        self.led_morse.off()
+
+    def _pause_word(self):
+        time.sleep_ms(self.difficulty*7)
+
+    def _pause_intra_char(self):
+        time.sleep_ms(self.difficulty)
+
+    def _pause_inter_char(self):
+        time.sleep_ms(self.difficulty*3)
+
+    def _write_morse(self, sentence):
+        sentence = sentence.lower()
+        morse_sentence = [self.international_morse_code[x] for x in sentence]
+        for char in morse_sentence:
+            if "/" == char:
+                self._pause_word()
+            for x in range(len(char)):
+                if "." == char[x]:
+                    self._dot()
+                elif "-" == char[x]:
+                    self._dash()
+                if x == len(char)-1:
+                    self._pause_inter_char()
+                else:
+                    self._pause_intra_char()
+
+    def _get_wordlist(self):
+        with open("wordlist.txt") as fh:
+            words = [x.strip() for x in fh.readlines()]
+            if self.difficulty >= 160:
+                words = [x for x in words if len(x) == 5]
+            start_index = random.randint(0, len(words)-self.wordlist_count)
+            return words[start_index:start_index+self.wordlist_count]
+
+    def start(self):
+        # TODO Display current self.current frequency
+        while not self.solved:
+            self._write_morse(self.chosen_one)
+            self._pause_word()
+
+
+"""
+EASY = 400
+NORMAL = 160
+HARD = 80
+EXPERT = 40
+PREPARE_2_DIE = 20
+"""
+g = MorseReadGame(difficulty=160, wordlist_count=16)
+g.start()
