8a2c2370fa248b9d8af9f4082c1bca3ef8abe3de
myOmikron <accounts@omikron.dev>
2021-11-03 01:37:59 +0100

    Added send and req methods for BMP

--- src_cpp/lib/BMP/BMP.cpp
+++ src_cpp/lib/BMP/BMP.cpp
@@ -5,26 +5,264 @@
 #include <EventLoop.hpp>
 #include <CAN.h>
 
-std::map<int, std::function<void(int, bool, std::string)>> callbacks;
-std::map<int, std::string> slicedTransmissions;
+#define MASTER_DEVICE 0
+
+std::map<uint8_t, std::function<void(uint8_t, bool, std::vector<uint8_t>)>> callbacks;
+std::map<uint8_t, std::vector<uint8_t>> slicedTransmissions;
 std::set<int> customMSGTypes;
-int address;
-std::set<int> reqOnly {
+uint8_t address;
+std::set<uint8_t> reqOnly {
     RESET, INIT, START, DEFUSED, EXPLODED, REGISTER, STRIKE, DETONATE, 
     MARK_SOLVED, MARK_REACTIVATED, CHANGE_SERIAL_NO, BLACKOUT
 };
-std::set<int> dataOnly { CHANGE_TIMER };
+std::set<uint8_t> dataOnly { CHANGE_TIMER };
 
 
-void BMP::setAddress(int addr) {
+
+void BMP::setAddress(uint8_t addr) {
     address = addr;
 }
 
-void BMP::setCallback(MSG_TYPE msgType, std::function<void(int sender, bool isRTR, std::string data)> callback) {
-    callbacks.insert(std::pair<int, std::function<void(int, bool, std::string)>>(msgType, callback));
+long BMP::constructPacketId(uint8_t dst, MSG_TYPE msgType, bool isSliced = false) {
+    return (((address << 4) + dst << 8) + msgType << 1) + (int)!isSliced << 12;
 }
 
-bool BMP::validatePacket(int msgType, bool isRTR) {
+void BMP::reqRESET(uint8_t dst) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(dst, RESET), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::reqINIT(uint8_t dst) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(dst, INIT), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::reqSTART(uint8_t dst) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(dst, START), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::reqDEFUSED(uint8_t dst) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(dst, DEFUSED), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::reqEXPLODED(uint8_t dst) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(dst, EXPLODED), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::reqRTFM(uint8_t dst) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(dst, RTFM), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::sendRTFM(std::string manual) {
+    for(int i = 0; i < manual.length(); i++) {
+        if(i%8 == 0) {
+            if(manual.length() - 1 - i < 8) {
+                CAN.beginExtendedPacket(BMP::constructPacketId(MASTER_DEVICE, RTFM));
+            } else {
+                CAN.beginExtendedPacket(BMP::constructPacketId(MASTER_DEVICE, RTFM, true));
+            }
+        }
+        CAN.write((char)manual[i]);
+        if(i%8 == 7 || i == manual.length() - 1) {
+            CAN.endPacket();
+        }
+    }
+}
+
+void BMP::reqVERSION(uint8_t dst) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(dst, VERSION), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::sendVERSION(uint8_t dst, uint8_t version) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(dst, VERSION));
+    CAN.write((char)version);
+    CAN.endPacket();
+}
+
+void BMP::reqMODULE_INFO(uint8_t dst) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(dst, MODULE_INFO), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::sendMODULE_INFO(uint8_t dst, std::vector<uint8_t> data) {
+    // TODO
+}
+
+void BMP::reqREGISTER() {
+    CAN.beginExtendedPacket(BMP::constructPacketId(MASTER_DEVICE, REGISTER), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::reqSTRIKE() {
+    CAN.beginExtendedPacket(BMP::constructPacketId(MASTER_DEVICE, STRIKE), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::reqDETONATE() {
+    CAN.beginExtendedPacket(BMP::constructPacketId(MASTER_DEVICE, DETONATE), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::reqMARK_SOLVED() {
+    CAN.beginExtendedPacket(BMP::constructPacketId(MASTER_DEVICE, MARK_SOLVED), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::reqMARK_REACTIVATED() {
+    CAN.beginExtendedPacket(BMP::constructPacketId(MASTER_DEVICE, MARK_REACTIVATED), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::sendCHANGE_TIMER(int32_t deltaTime) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(MASTER_DEVICE, CHANGE_TIMER));
+    for(int i = 1; i < 5; i++) {
+        CAN.write(deltaTime >> i*8);
+    }
+    CAN.endPacket();
+}
+
+void BMP::reqCHANGE_SERIAL_NO() {
+    CAN.beginExtendedPacket(BMP::constructPacketId(MASTER_DEVICE, CHANGE_SERIAL_NO), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::reqTIMER() {
+    CAN.beginExtendedPacket(BMP::constructPacketId(MASTER_DEVICE, TIMER), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::sendTIMER(uint8_t dst, uint32_t timer) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(dst, TIMER));
+    for(int i = 5; i > 0; i--) {
+        CAN.write(timer >> i*8);
+    }
+    CAN.endPacket();
+}
+
+void BMP::reqSERIAL_NO() {
+    CAN.beginExtendedPacket(BMP::constructPacketId(MASTER_DEVICE, SERIAL_NO), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::sendSERIAL_NO(uint8_t dst, std::string serialNo) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(dst, SERIAL_NO));
+    for(int i = 0; i < serialNo.length(); i++) {
+        if(i >= 8) {
+            break;
+        }
+        CAN.write((char)serialNo[i]);
+    }
+    CAN.endPacket();
+}
+
+void BMP::reqSTRIKES() {
+    CAN.beginExtendedPacket(BMP::constructPacketId(MASTER_DEVICE, STRIKES), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::sendSTRIKES(uint8_t dst, uint8_t strikes) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(dst, STRIKES));
+    CAN.write(strikes);
+    CAN.endPacket();
+}
+
+void BMP::reqMAX_STRIKES() {
+    CAN.beginExtendedPacket(BMP::constructPacketId(MASTER_DEVICE, MAX_STRIKES), -1, true);
+    CAN.endPacket();
+}
+void BMP::sendMAX_STRIKES(uint8_t dst, uint8_t maxStrikes) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(dst, MAX_STRIKES));
+    CAN.write(maxStrikes);
+    CAN.endPacket();
+}
+
+void BMP::reqMOUDLE_COUNT() {
+    CAN.beginExtendedPacket(BMP::constructPacketId(MASTER_DEVICE, MODULE_COUNT), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::sendMODULE_COUNT(uint8_t dst, uint8_t moduleCount) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(dst, MODULE_COUNT));
+    CAN.write(moduleCount);
+    CAN.endPacket();
+}
+
+void BMP::reqACTIVE_MODULE_COUNT() {
+    CAN.beginExtendedPacket(BMP::constructPacketId(MASTER_DEVICE, ACTIVE_MODULE_COUNT), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::sendACTIVE_MODULE_COUNT(uint8_t dst, uint8_t activeModuleCount) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(dst, ACTIVE_MODULE_COUNT));
+    CAN.write(activeModuleCount);
+    CAN.endPacket();
+}
+
+void BMP::reqDIFFICULTY() {
+    CAN.beginExtendedPacket(BMP::constructPacketId(MASTER_DEVICE, DIFFICULTY), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::sendDIFFICULTY(uint8_t dst, uint8_t difficulty) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(dst, DIFFICULTY));
+    CAN.write(difficulty);
+    CAN.endPacket();
+}
+
+void BMP::reqLABELS() {
+    CAN.beginExtendedPacket(BMP::constructPacketId(MASTER_DEVICE, LABELS), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::sendLABELS(uint8_t dst, std::map<std::string, bool> labels) {
+    int i = 0;
+    for(auto pair : labels) {
+        if(i%2 == 0) {
+            if(labels.size() > 2) {
+                CAN.beginExtendedPacket(BMP::constructPacketId(dst, LABELS, true));
+            } else {
+                CAN.beginExtendedPacket(BMP::constructPacketId(dst, LABELS));
+            }
+        }
+        CAN.write(pair.second);
+        for(int j = 0; j < pair.first.length(); j++) {
+            CAN.write((char)pair.first[j]);
+        }
+
+        if(i%2 == 1 || i == labels.size() - 1) {
+            CAN.endPacket();
+        }
+
+        i++;
+    }
+}
+
+void BMP::reqBLACKOUT(uint8_t dst) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(dst, BLACKOUT), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::reqIS_SOLVED(uint8_t dst) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(dst, IS_SOLVED), -1, true);
+    CAN.endPacket();
+}
+
+void BMP::sendIS_SOLVED(uint8_t dst, bool isSolved) {
+    CAN.beginExtendedPacket(BMP::constructPacketId(dst, IS_SOLVED));
+    CAN.write(isSolved);
+    CAN.endPacket();
+}
+
+void BMP::setCallback(uint8_t msgType, std::function<void(uint8_t sender, bool isRTR, std::vector<uint8_t> data)> callback) {
+    callbacks.insert(std::pair<int, std::function<void(uint8_t, bool, std::vector<uint8_t>)>>(msgType, callback));
+}
+
+bool BMP::validatePacket(uint8_t msgType, bool isRTR) {
     // REQ only
     if(reqOnly.count(msgType) > 0) {
         return isRTR ? true : false;
@@ -67,20 +305,24 @@ void BMP::processInterrupt(int packetSize) {
         if(!(header & 0b0000000000000001000000000000) >> 12) {
             return;
         }
-        addJob(millis(), [msgType, sender](){callbacks.at(msgType)(sender, true, "");});
+        addJob(millis(), [msgType, sender](){callbacks.at(msgType)(sender, true, std::vector<uint8_t>());});
     } else {
         // SENDER + RECEIVER + MSG_TYPE as "unique" packet identifier
         int identifier = header >> 13;
 
-        std::string data = "";
+        std::vector<uint8_t> data;
+
         for (int i = 0; i < packetSize; i++) {
-            data += (char)CAN.read();
+            data.push_back((uint8_t)CAN.read());
         }
         //if EOT
         if((header & 0b0000000000000001000000000000) >> 12) {
             // Check if packet is part of a sliced transmission
             if(slicedTransmissions.count(identifier)) {
-                std::string completeData = slicedTransmissions.at(identifier) + data;
+                std::vector<uint8_t> completeData = slicedTransmissions.at(identifier);
+                for(auto i : data) {
+                    completeData.push_back(i);
+                }
                 slicedTransmissions.erase(identifier);
                 addJob(millis(), [msgType, sender, completeData](){callbacks.at(msgType)(sender, false, completeData);});
             } else {
@@ -88,9 +330,11 @@ void BMP::processInterrupt(int packetSize) {
             }
         } else {
             if(slicedTransmissions.count(identifier)) {
-                slicedTransmissions.at(identifier) += data;
+                for(auto i : data) {
+                    slicedTransmissions.at(identifier).push_back(i);
+                }
             } else {
-                slicedTransmissions.insert(std::pair<int, std::string>(identifier, data));
+                slicedTransmissions.insert(std::pair<int, std::vector<uint8_t>>(identifier, data));
             }
         }
     }
--- src_cpp/lib/BMP/BMP.hpp
+++ src_cpp/lib/BMP/BMP.hpp
@@ -2,7 +2,7 @@
 #define BMP_HPP_
 
 #include <functional>
-#include <string>
+#include <vector>
 
 enum MSG_TYPE {
     RESET = 0, INIT = 1, START = 2, DEFUSED = 3, EXPLODED = 4, RTFM = 5, VERSION = 6, MODULE_INFO = 7, 
@@ -13,13 +13,51 @@ enum MSG_TYPE {
 
 class BMP {
     public:
-        static void setCallback(MSG_TYPE msgType, std::function<void(int sender, bool isRTR, std::string data)> callback);
         static void begin(long baudrate);
+        static void setAddress(uint8_t addr);
+        static void setCallback(uint8_t msgType, std::function<void(uint8_t sender, bool isRTR, std::vector<uint8_t> data)> callback);
         static void registerCustomMSGType(uint8_t msgType);
-        static void setAddress(int addr);
+        static void reqRESET(uint8_t dst);
+        static void reqINIT(uint8_t dst);
+        static void reqSTART(uint8_t dst);
+        static void reqDEFUSED(uint8_t dst);
+        static void reqEXPLODED(uint8_t dst);
+        static void reqRTFM(uint8_t dst);
+        static void sendRTFM(std::string);
+        static void reqVERSION(uint8_t dst);
+        static void sendVERSION(uint8_t dst, uint8_t version);
+        static void reqMODULE_INFO(uint8_t dst);
+        static void sendMODULE_INFO(uint8_t dst, std::vector<uint8_t> data);
+        static void reqREGISTER();
+        static void reqSTRIKE();
+        static void reqDETONATE();
+        static void reqMARK_SOLVED();
+        static void reqMARK_REACTIVATED();
+        static void sendCHANGE_TIMER(int32_t deltaTime);
+        static void reqCHANGE_SERIAL_NO();
+        static void reqTIMER();
+        static void sendTIMER(uint8_t dst, uint32_t timer);
+        static void reqSERIAL_NO();
+        static void sendSERIAL_NO(uint8_t dst, std::string);
+        static void reqSTRIKES();
+        static void sendSTRIKES(uint8_t dst, uint8_t strikes);
+        static void reqMAX_STRIKES();
+        static void sendMAX_STRIKES(uint8_t dst, uint8_t maxStrikes);
+        static void reqMOUDLE_COUNT();
+        static void sendMODULE_COUNT(uint8_t dst, uint8_t moduleCount);
+        static void reqACTIVE_MODULE_COUNT();
+        static void sendACTIVE_MODULE_COUNT(uint8_t dst, uint8_t activeModuleCount);
+        static void reqDIFFICULTY();
+        static void sendDIFFICULTY(uint8_t dst, uint8_t difficulty);
+        static void reqLABELS();
+        static void sendLABELS(uint8_t dst, std::map<std::string, bool> labels);
+        static void reqBLACKOUT(uint8_t dst);
+        static void reqIS_SOLVED(uint8_t dst);
+        static void sendIS_SOLVED(uint8_t dst, bool isSolved);
     private:
+        static long constructPacketId(uint8_t dst, MSG_TYPE msgType, bool isSliced=false);
         static void processInterrupt(int packetSize);
-        static bool validatePacket(int msgType, bool isRTR);
+        static bool validatePacket(uint8_t msgType, bool isRTR);
 };
 
 #endif
